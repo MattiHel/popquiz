99b8d9b84ae7c7af18d93c7529e85329a2582e18
Matthew Simpson <matti.simpson@hotmail.fi>
2020-03-29 13:52:42 +0300

    Create quiz-config.js

---
+++ javascript/quiz-config.js
@@ -0,0 +1,127 @@
+quiz.questions = [ 
+    {
+        question: "Which actor portrayed the first live action Superman?",
+        answers: {
+            a: "George Reeves",
+            b: "Christopher Reeve",
+            c: "Kirk Alyn"
+        },
+        correctAnswer: 'c'
+    },
+    
+    {
+        question: "Who created the character Spawn?",
+        answers: {
+            a: "Todd McFarlane",
+            b: "Jack Kirby",
+            c: "Rob Liefeld"
+        },
+        correctAnswer: 'a'
+    },
+
+    {
+        question: "Which Kevin Smith movie is not set in the 'View Askewniverse'?",
+        answers: {
+            a: "Clerks",
+            b: "Mallrats",
+            c: "Jersey Girl"
+        },
+        correctAnswer: 'c'
+    },
+    
+    {
+        question: "Who played Daisy Domergue in Quentin Tarantino's 'The Hateful Eight'?",
+        answers: {
+            a: "Jennifer Love Hewitt",
+            b: "Jason Lee",
+            c: "Jennifer Jason Leigh"
+        },
+        correctAnswer: 'c'
+    },
+
+    {
+        question: "Who wrote the 2004 teen comedy 'Mean Girls?",
+        answers: {
+            a: "Tina Fey",
+            b: "Larry David",
+            c: "Emma Thompson"
+        },
+        correctAnswer: 'a'
+    },
+
+    {
+        question: "What did Farva want to name his and Ramathorn's car in 'Super Troopers'?",
+        answers: {
+            a: "Leader Cola",
+            b: "Team Ramrod",
+            c: "Johnny Chimpo"
+        },
+        correctAnswer: 'b'
+    },
+
+    {
+        question: "How much money did Earl win before being hit by a car in 'My Name Is Earl'?",
+        answers: {
+            a: "$500,000",
+            b: "$250,000",
+            c: "$100,000"
+        },
+        correctAnswer: 'c'
+    },
+
+    {
+        question: "What is the name of Snowman's dog in 'Smokey and the Bandit'?",
+        answers: {
+            a: "Larry",
+            b: "Fred",
+            c: "Burt"
+        },
+        correctAnswer: 'b'
+    },
+
+    {
+        question: "Which film director is rumoured to have filmed the moon landing?",
+        answers: {
+            a: "Stanley Kubrick",
+            b: "Ingmar Bergman",
+            c: "Roman Polanski"
+        },
+        correctAnswer: 'a'
+    },
+
+    {
+        question: "Will Smith turned down the role of Neo in 'The Matrix' to make which film?",
+        answers: {
+            a: "Men In Black 2",
+            b: "Wild Wild West",
+            c: "Hitch"
+        },
+        correctAnswer: 'b'
+    }
+    
+    
+
+];
+
+quiz.verdicts = [
+    {
+        minScore: 100,
+        text: " Alright, alright, alright!"
+    },
+    {
+        minScore: 50,
+        text: " Pretty, pretty, prettaay good!"
+    },
+    {
+        minScore: 33,
+        text: " Think McFly, Think!"
+    },
+    {
+        minScore: 15,
+        text: " Fork! Me can't eat soup!"
+    },
+    {
+        minScore: 0,
+        text: " Wrong wrong wrong, wrong wrong wrong wrong! You're wrong! you're wrong! YOU'RE WROOONG!!!"
+    }
+];
